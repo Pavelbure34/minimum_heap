54be2e94f06ebf39d4b8442f86c637ef501a65c2
Pavelbure34 <dongbinsuh@gmail.com>
2019-09-29 15:57:42 -0400

    data size adjusted

--- timesort.cpp
+++ timesort.cpp
@@ -2,34 +2,34 @@
 #include "heap.h"
 
 int main(){
-    int *testArr1 = new int[50];
-    int *testArr2 = new int[75];
-    int *testArr3 = new int[100];
-    int *testArr4 = new int[125];
-    int *testArr5 = new int[150];
-    int *testArr6 = new int[175];
-    int *testArr7 = new int[200];
+    int *testArr1 = new int[100];
+    int *testArr2 = new int[250];
+    int *testArr3 = new int[500];
+    int *testArr4 = new int[750];
+    int *testArr5 = new int[1000];
+    int *testArr6 = new int[1250];
+    int *testArr7 = new int[1500];
 
-    for (int i = 0; i <  50; i++){
-      testArr1[i] = 50 - i;
-    }
-    for (int i = 0; i <  75; i++){
-      testArr2[i] = 75 - i;
-    }
     for (int i = 0; i <  100; i++){
-      testArr3[i] = 100 - i;
+      testArr1[i] = 100 - i;
     }
-    for (int i = 0; i <  125; i++){
-      testArr4[i] = 125 - i;
+    for (int i = 0; i <  250; i++){
+      testArr2[i] = 250 - i;
     }
-    for (int i = 0; i <  150; i++){
-      testArr5[i] = 150 - i;
+    for (int i = 0; i < 500; i++){
+      testArr3[i] = 500 - i;
     }
-    for (int i = 0; i <  175; i++){
-      testArr6[i] = 175 - i;
+    for (int i = 0; i < 750; i++){
+      testArr4[i] = 750 - i;
     }
-    for (int i = 0; i <  200; i++){
-      testArr7[i] = 200 - i;
+    for (int i = 0; i < 1000; i++){
+      testArr5[i] = 1000 - i;
+    }
+    for (int i = 0; i < 1250; i++){
+      testArr6[i] = 1250 - i;
+    }
+    for (int i = 0; i < 1500; i++){
+      testArr7[i] = 1500 - i;
     }
 
     //set up here
@@ -40,7 +40,7 @@ int main(){
     gettimeofday(&timeBefore, NULL);
 
     //call sorting method here.
-    MinHeap<int> test1(testArr1, 50);
+    MinHeap<int> test1(testArr1, 100);
     test1.heapSort(testArr1);
 
     gettimeofday(&timeAfter, NULL);
@@ -53,7 +53,7 @@ int main(){
     gettimeofday(&timeBefore, NULL);
 
     //call sorting method here.
-    MinHeap<int> test2(testArr2, 75);
+    MinHeap<int> test2(testArr2, 250);
     test1.heapSort(testArr2);
 
     gettimeofday(&timeAfter, NULL);
@@ -66,7 +66,7 @@ int main(){
     gettimeofday(&timeBefore, NULL);
 
     //call sorting method here.
-    MinHeap<int> test3(testArr3, 100);
+    MinHeap<int> test3(testArr3, 500);
     test1.heapSort(testArr3);
 
     gettimeofday(&timeAfter, NULL);
@@ -79,7 +79,7 @@ int main(){
     gettimeofday(&timeBefore, NULL);
 
     //call sorting method here.
-    MinHeap<int> test4(testArr4, 125);
+    MinHeap<int> test4(testArr4, 750);
     test1.heapSort(testArr4);
 
     gettimeofday(&timeAfter, NULL);
@@ -92,7 +92,7 @@ int main(){
     gettimeofday(&timeBefore, NULL);
 
     //call sorting method here.
-    MinHeap<int> test5(testArr5, 150);
+    MinHeap<int> test5(testArr5, 1000);
     test1.heapSort(testArr5);
 
     gettimeofday(&timeAfter, NULL);
@@ -105,7 +105,7 @@ int main(){
     gettimeofday(&timeBefore, NULL);
 
     //call sorting method here.
-    MinHeap<int> test6(testArr6, 175);
+    MinHeap<int> test6(testArr6, 1250);
     test1.heapSort(testArr6);
 
     gettimeofday(&timeAfter, NULL);
@@ -118,7 +118,7 @@ int main(){
     gettimeofday(&timeBefore, NULL);
 
     //call sorting method here.
-    MinHeap<int> test7(testArr7, 200);
+    MinHeap<int> test7(testArr7, 1500);
     test1.heapSort(testArr7);
 
     gettimeofday(&timeAfter, NULL);
